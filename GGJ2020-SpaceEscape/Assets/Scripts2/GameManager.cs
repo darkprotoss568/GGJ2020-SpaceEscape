843cd1d6c2f286cafde244402f46ea2f2f7c0008
darkprotoss568 <darkprotoss568@gmail.com>
2020-02-01 14:30:34 +1100

    Added music controls

---
+++ GGJ2020-SpaceEscape/Assets/Scripts2/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager Instance;
+	[SerializeField]
+	private Transform AnswerFrameArea;
+	[SerializeField]
+	private Transform ObjectsFrameArea;
+	[SerializeField]
+	private LevelData[] levels;
+	private int currentLevel = -1;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (GameManager.Instance != null)
+			Destroy(gameObject);
+		else
+			Instance = this;
+	}
+	
+	void Start()
+	{
+		LoadNewLevel();
+	}
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	
+	public void LoadNewLevel()
+	{
+		currentLevel ++;
+		LevelData level = levels[currentLevel];
+		MusicGameplayManager.Instance.SetCurrentSoundTrack(level.soundTrack);
+		List<AudioClip> allAnswers = MusicGameplayManager.Instance.CreateNewAnswerSet(level.correctAnswers, level.allAnswers);
+		
+	}
+}
+
+
+[System.Serializable]
+public struct LevelData
+{
+	public AudioClip soundTrack;
+	public int correctAnswers;
+	public int allAnswers;
+}
---
+++ GGJ2020-SpaceEscape/Assets/Scripts2/RepairObjScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepairObjScript : MonoBehaviour
+{
+    AudioClip answerClip = null;
+
+    public AudioClip AnswerClip
+    {
+        get
+        {
+            return answerClip;
+        }
+
+        set
+        {
+            answerClip = value;
+        }
+    }
+    // Start is called before the first frame update
+
+
+}
