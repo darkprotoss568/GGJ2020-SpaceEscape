d2f17047b8477e1e90a6ec6e93d8af410b0fd942
darkprotoss568 <darkprotoss568@gmail.com>
2020-02-09 11:40:10 +1100

    Added main menu with level load scripts

---
+++ GGJ2020-SpaceEscape/Assets/Scripts2/MainMenuScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+
+    public void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
